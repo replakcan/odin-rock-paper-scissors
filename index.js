8fa2878beb508efc5ed9c0ead4808c791ca6515c
replakcan <alper.makcan@gmail.com>
2025-03-06 22:55:33 +0300

    Updates rps logic with DOM manipulation

--- index.js
+++ index.js
@@ -9,36 +9,55 @@ const getHumanChoice = () => {
   return prompt('Make your move!')
 }
 
-const playGame = () => {
-  let humanScore = 0
-  let computerScore = 0
+let humanScore = 0
+let computerScore = 0
 
-  const playRound = (humanChoice, computerChoice) => {
-    if (humanChoice == computerChoice) return
+const playRound = (humanChoice, computerChoice) => {
+  if (humanChoice == computerChoice) return
 
-    if (humanChoice == 'paper') {
-      computerChoice == 'rock' ? humanScore++ : computerScore++
-    } else if (humanChoice == 'rock') {
-      computerChoice == 'paper' ? computerScore++ : humanScore++
-    } else if (humanChoice == 'scissors') {
-      computerChoice == 'paper' ? humanScore++ : computerScore++
-    }
-    console.log(`human: ${humanScore} computer: ${computerScore}`)
+  if (humanChoice == 'paper') {
+    computerChoice == 'rock' ? humanScore++ : computerScore++
+  } else if (humanChoice == 'rock') {
+    computerChoice == 'paper' ? computerScore++ : humanScore++
+  } else if (humanChoice == 'scissors') {
+    computerChoice == 'paper' ? humanScore++ : computerScore++
   }
-
-  for (let i = 0; i < 5; i++) {
-    let humanSelection = getHumanChoice().toLowerCase()
-    console.log(humanSelection)
-    let computerSelection = getComputerChoice()
-    console.log(computerSelection)
-    playRound(humanSelection, computerSelection)
-  }
-
-  humanScore == computerScore
-    ? console.log('it is a DRAW!')
-    : humanScore > computerScore
-    ? console.log('human won!')
-    : console.log('computer won!')
 }
 
-playGame()
+const body = document.querySelector('body')
+const btnRock = document.createElement('button')
+const btnPaper = document.createElement('button')
+const btnScissors = document.createElement('button')
+
+const btnGroup = document.createElement('div')
+btnGroup.classList.add('btn-group')
+body.appendChild(btnGroup)
+
+btnRock.textContent = 'Rock'
+btnPaper.textContent = 'Paper'
+btnScissors.textContent = 'Scissors'
+
+const btnArray = [btnRock, btnPaper, btnScissors]
+const resultDiv = document.createElement('div')
+body.appendChild(resultDiv)
+resultDiv.textContent = `human: ${humanScore}, computer: ${computerScore}`
+
+const clickEventFn = (e) => {
+  const humanSelection = e.target.textContent.toLowerCase()
+  const computerSelection = getComputerChoice()
+  playRound(humanSelection, computerSelection)
+}
+for (let i = 0; i < 3; i++) {
+  btnGroup.appendChild(btnArray[i])
+  btnArray[i].addEventListener('click', (e) => {
+    clickEventFn(e)
+    resultDiv.textContent = `human: ${humanScore}, computer: ${computerScore}`
+
+    if (humanScore == 5 || computerScore == 5) {
+      alert(`${humanScore == 5 ? 'human' : 'computer'} is the winner!`)
+      humanScore = 0
+      computerScore = 0
+      resultDiv.textContent = `human: ${humanScore}, computer: ${computerScore}`
+    }
+  })
+}
